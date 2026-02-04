e642dddf21ad09b0ef94d960402bb058b3407a40
NanoToolz <theasimgraphics@gmail.com>
2026-02-04 19:26:37 +0500

    Updated __init__.py

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,4 +1,3 @@
-from .app import create_dispatcher
-from .commands import set_bot_commands
+from .app import start_bot
 
-__all__ = ["create_dispatcher", "set_bot_commands"]
+__all__ = ["start_bot"]
